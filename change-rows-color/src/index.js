b06e5f1c63dd31c9ea5480c25f594450960dac4e
许要军 <xuyaojun@cdqcp.com>
2022-05-18 08:29:12 +0800

    完善其它loader示例

--- change-rows-color/src/index.js
+++ change-rows-color/src/index.js
@@ -1,7 +1,18 @@
 // 1. 使用 ES6 导入语法，导入 jQuery
 import $ from 'jquery'
 
-$(function(){
+//导入样式（在webpack中，一切皆模块，都可以
+import a from './css/index.css'
+import b from './css/index.less'
+console.log(a)
+//导入图片，得到图片文件。
+
+import logo from './images/logo.jpg'
+//给img标签的src属性赋值。
+$(".box").attr('src', logo)
+
+
+$(function () {
   // 奇数行为红色 green yellow
   $('li:odd').css('background-color', 'green');
   $('li:even').css('background-color', 'blue');
@@ -10,3 +21,15 @@ $(function(){
   // 1 是奇数
 
 })
+
+
+// 定义装饰器函数
+function info(target) {
+  target.info = 'Person info.'
+}
+
+// 定义一个普通的类
+@info
+class Person { }
+
+console.log(Person.info)
--- change-rows-color/webpack.config.js
+++ change-rows-color/webpack.config.js
@@ -8,7 +8,17 @@ const htmlPlugin = new HtmlPlugin({
 })
 
 
+// 注意：左侧的 { } 是解构赋值
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
+
+    // 在开发调试阶段，建议大家都把 devtool 的值设置为 eval-source-map
+    // devtool: 'eval-source-map',
+    // 在实际发布的时候，建议大家把 devtool 的值设置为 nosources-source-map 或直接关闭 SourceMap
+    devtool: 'eval-source-map',
+
+
     mode: 'development',//mode 用来指定构建模式，可选值有development和production
     devServer: {
         open: true,
@@ -17,8 +27,30 @@ module.exports = {
     },
     entry: path.join(__dirname, './src/index.js'), //打包入口文件的路径。
     output: {
-        path: path.join(__dirname, './dist'), //输出文件的存放路径。
-        filename: 'bundle.js'
+        path: path.join(__dirname, 'dist'), //输出文件的存放路径。
+        filename: 'js/bundle.js'
     },
-    plugins: [htmlPlugin], //3.通过plugins节点，使用hmltPlugin插件生效。
+    plugins: [htmlPlugin, new CleanWebpackPlugin()], //3.通过plugins节点，使用hmltPlugin插件生效。
+    module: {
+        rules: [
+            // 定义了不同模块对应的 loader
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            // 处理 .less 文件的 loader
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+            // 处理图片文件的 loader
+            // 如果需要调用的 loader 只有一个，则只传递一个字符串也行，如果有多个loader，则必须指定数组
+            // 在配置 url-loader 的时候，多个参数之间，使用 & 符号进行分隔
+            { test: /\.jpg|png|gif$/, use: 'url-loader?limit=470&outputPath=images' },
+            // 使用 babel-loader 处理高级的 JS 语法
+            // 在配置 babel-loader 的时候，程序员只需要把自己的代码进行转换即可；一定要排除 node_modules 目录中的 JS 文件
+            // 因为第三方包中的 JS 兼容性，不需要程序员关心
+            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ }
+        ]
+    },
+    resolve: {
+        alias: {
+            // 告诉 webpack，程序员写的代码中，@ 符号表示 src 这一层目录
+            '@': path.join(__dirname, './src/')
+        }
+    }
 }
